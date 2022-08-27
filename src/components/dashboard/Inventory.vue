0136f97ab409aa04da72214693416a364ed7ff25
momrider69 <jacob.abraham@claystack.com>
2022-08-27 22:13:46 +0530

    Style table; todo expand layout

--- src/components/dashboard/Inventory.vue
+++ src/components/dashboard/Inventory.vue
@@ -2,23 +2,71 @@
   <div class="flex flex-col mt-8">
     <span class="font-bold text-sm">Inventory</span>
     <table class="table-fixed">
-      <thead>
-        <tr class="text-grey-20 text-sm">
-          <th><input type="checkbox" /></th>
-          <th>Product</th>
-          <th>Rating</th>
-          <th>Vintage</th>
-          <th>QTY</th>
-          <th>Volume</th>
-          <th>Cost</th>
-          <th>Price</th>
+      <thead class="table-header-group border-b-[1px] border-gray-10 h-16">
+        <tr class="text-grey-20 text-sm text-center">
+          <th><input type="checkbox" :onchange="toggleSelectAll" /></th>
+          <th class="cursor-pointer">
+            <span>Product</span>
+            <Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            Rating<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            Vintage<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            QTY<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            Volume<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            Cost<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
+          <th class="cursor-pointer">
+            Price<Icon size="16" class="text-gray-20 ml-1"
+              ><ArrowSort16Regular
+            /></Icon>
+          </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in inventoryItems">
-          <th><input type="checkbox" /></th>
-          <td>{{ item.name }}</td>
-          <td>{{ item.rating.robert }}</td>
+        <tr
+          v-for="(item, index) in inventoryItems"
+          class="text-center h-16 border-b-[1px] border-gray-10 text-gray-20 text-sm"
+        >
+          <th>
+            <input type="checkbox" :checked="selectAll" />
+          </th>
+          <td>
+            <div class="flex space-x-2 justify-left ml-8">
+              <div class="w-12 h-12 bg-gray-10 rounded-sm"></div>
+              <div class="flex flex-col items-start">
+                <span class="text-base font-semibold text-[#262730]">{{
+                  item.name
+                }}</span>
+                <span class="text-xs text-gray-20">{{ item.region }}</span>
+              </div>
+            </div>
+          </td>
+          <td class="text-gray-30 text-sm">
+            <span class="px-2 py-[2px] bg-gray-10 rounded-md">{{
+              item.rating.robert
+            }}</span>
+          </td>
           <td>{{ item.vintage }}</td>
           <td>
             {{
@@ -30,6 +78,20 @@
           <td>{{ item.volume }}</td>
           <td>{{ item.cost }}</td>
           <td>{{ item.price }}</td>
+          <td class="cursor-pointer" v-on:click="() => onExpand(index)">
+            <Icon
+              size="16"
+              v-if="expandedItem.id == index && expandedItem.isExpanded"
+            >
+              <ChevronUpOutline />
+            </Icon>
+            <Icon
+              size="16"
+              v-if="!expandedItem.isExpanded || expandedItem.id != index"
+            >
+              <ChevronDownOutline />
+            </Icon>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -38,9 +100,12 @@
 
 <script lang="ts">
 import { ArrowSort16Regular } from "@vicons/fluent";
+import { ChevronDownOutline, ChevronUpOutline } from "@vicons/ionicons5";
 import { Icon } from "@vicons/utils";
 export default {
   components: {
+    ChevronUpOutline,
+    ChevronDownOutline,
     ArrowSort16Regular,
     Icon,
   },
@@ -138,7 +203,22 @@ export default {
           price: "$72.95",
         },
       ],
+      expandedItem: { id: 0, isExpanded: false },
+      selectAll: false,
     };
+  },
+  methods: {
+    onExpand(id: number) {
+      //@ts-expect-error
+      let currentExpansion = this.expandedItem.isExpanded;
+      //@ts-expect-error
+      this.expandedItem = { id, isExpanded: !currentExpansion };
+      console.log(id);
+    },
+    toggleSelectAll() {
+      //@ts-expect-error
+      this.selectAll = !this.selectAll;
+    },
   },
 };
 </script>
